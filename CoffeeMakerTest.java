482ddb86fd81f71d005d5e9b80e32b54de843ebf
BrendanMurray <thebmurr@gmail.com>
2016-02-10 11:06:53 -0500

    added drink() and look() test cases
    still have some drink() cases to add before it is covered; these tests
    provide full coverage of look()

--- CoffeeMakerTest.java
+++ CoffeeMakerTest.java
@@ -1,12 +1,11 @@
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CoffeeMakerTest
 {
-	//--Movement Tests--//
-	
-	//test moveNorth
 	@Test
 	public void northTest()
 	{
@@ -44,5 +43,57 @@ public class CoffeeMakerTest
 		assertEquals(coffeeQ.getRoomNum(), 0); //Make sure can't move past first room
 	}
 	
-	//
+	@Test
+	public void testDrinkAllItems(){
+		CoffeeMakerQuest testCQ = new CoffeeMakerQuest();
+		
+		testCQ.setCoffee(true);
+		testCQ.setCream(true);
+		testCQ.setSugar(true);
+		int drinkCode = testCQ.drink();
+		assertEquals(0,drinkCode);
+	}
+	
+	@Test
+	public void testLookCream(){
+		CoffeeMakerQuest testCQ = new CoffeeMakerQuest();
+		
+		testCQ.setRoom(0);
+		testCQ.look();
+		assertEquals(testCQ.getCream(),true);
+	}
+	
+	@Test
+	public void testLookCoffee(){
+		CoffeeMakerQuest testCQ = new CoffeeMakerQuest();
+		
+		testCQ.setRoom(2);
+		testCQ.look();
+		assertEquals(testCQ.getCoffee(),true);
+	}
+	
+	@Test
+	public void testLookSugar(){
+		CoffeeMakerQuest testCQ = new CoffeeMakerQuest();
+		
+		testCQ.setRoom(5);
+		testCQ.look();
+		assertEquals(testCQ.getSugar(),true);
+	}
+	
+	@Test
+	public void testLookEmpty(){
+		CoffeeMakerQuest testCQ = new CoffeeMakerQuest();
+		testCQ.setRoom(1);
+		testCQ.look();
+		testCQ.setRoom(3);
+		testCQ.look();
+		testCQ.setRoom(4);
+		testCQ.look();
+		assertEquals(testCQ.getCream(),false);
+		assertEquals(testCQ.getCoffee(),false);
+		assertEquals(testCQ.getSugar(),false);
+	}
+
 }
+
